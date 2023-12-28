78efadbd03aec5c401e0683369f075d9cfbb3dd1
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-28 19:08:16 +0700

    Add insert kontak on KontakRepository

--- app/src/main/java/com/example/activity8/repository/KontakRepository.kt
+++ app/src/main/java/com/example/activity8/repository/KontakRepository.kt
@@ -6,10 +6,18 @@ import com.example.activity8.service_api.KontakService
 interface KontakRepository{
     suspend fun getKontak(): List<Kontak>
 
+    suspend fun insertKontak(kontak: Kontak)
+
+
+
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    override suspend fun insertKontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+    }
 }
