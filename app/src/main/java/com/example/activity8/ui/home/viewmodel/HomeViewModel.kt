95b3d9e501e6cb4acdc57c29957761fac8872848
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-28 19:45:00 +0700

    Add function deleteKontak in HomeViewModel

--- app/src/main/java/com/example/activity8/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/activity8/ui/home/viewmodel/HomeViewModel.kt
@@ -43,4 +43,16 @@ class HomeViewModel (private val kontakRepository: KontakRepository) : ViewModel
             }
         }
     }
+
+    fun deleteKontak(id: Int) {
+        viewModelScope.launch {
+            try {
+                kontakRepository.deleteKontak(id)
+            } catch (e: IOException) {
+                KontakUIState.Error
+            } catch (e: HttpException) {
+                KontakUIState.Error
+            }
+        }
+    }
 }
