90f4f516783a42ad88006d5a6fa72b35305e78a6
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-28 21:13:58 +0700

    Add data class InsertUiState on insertviewmodel

--- app/src/main/java/com/example/activity8/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/activity8/ui/home/viewmodel/InsertViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.activity8.ui.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.activity8.model.Kontak
 import com.example.activity8.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository): ViewModel(){
 
 }
+
+data class InsertUiState(
+    val insertUiState: InsertUiState = InsertUiState(),
+)
