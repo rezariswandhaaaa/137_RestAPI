88f61007d323b1dc0800a195bc5bd77fe82b6dfd
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-28 21:35:13 +0700

    Add fun updateInsertkontak and insertKontak in insertviewmodel

--- app/src/main/java/com/example/activity8/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/activity8/ui/home/viewmodel/InsertViewModel.kt
@@ -1,15 +1,36 @@
 package com.example.activity8.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.activity8.model.Kontak
 import com.example.activity8.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository): ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertUiState(
-    val insertUiState: InsertUiState = InsertUiState(),
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
 
 data class InsertUiEvent(
@@ -17,4 +38,22 @@ data class InsertUiEvent(
     val nama: String = "",
     val email: String = "",
     val nohp: String = "",
+)
+
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = email,
+    telpon = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = alamat,
+    nohp = telpon
 )
