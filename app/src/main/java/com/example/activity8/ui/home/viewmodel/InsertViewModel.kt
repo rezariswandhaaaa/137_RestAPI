a42ec393a39d28f745e1c4fa41147e0c69f6fefa
Reza-Riswandha <emrezariswanda@gmail.com>
2023-12-28 21:11:04 +0700

    Create InsertViewModel on package viewmodel

---
+++ app/src/main/java/com/example/activity8/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.activity8.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.activity8.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository): ViewModel(){
+
+}
